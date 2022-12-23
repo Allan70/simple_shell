5610855aa50a057e66f5b370e608d31c93ba9fff
Allan70 <aabere70@gmail.com>
2022-12-23 00:30:19 -0800

    parsing done

---
+++ parse.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define SHELL_TOKEN_BUFFER_SIZE 64
+#define SHELL_TOKEN_DELIMITER " \t\r\n\a"
+
+/**
+ * shell_split_line - This function parses the line in our shell
+ *
+ * @line: The function takes the nine from he shell as input
+ *
+ * Return: A tiken is returned
+ */
+
+char **shell_split_line(char *line)
+{
+	int buffer_size = SHELL_TOKEN_BUFFER_SIZE, position = 0;
+	char **tokens = malloc(buffer_size * sizeof(char *));
+	char *token;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "shell: allocation error\n");
+		exit(EXIT_FAILIURE);
+	}
+
+	token = strtok(line, SHELL_TOKEN_DELIMITER);
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+
+		if (position >= buffer_size)
+		{
+			buffer_size += SHELL_TOKEN_BUFFER_SIZE;
+			tokens = realloc(tokens, buffer_size * sizeof(char *));
+			if (!tokens)
+			{
+				fprintf(stderr, "shell: allocation error\n");
+				exit(EXIT_FAILIURE);
+			}
+		}
+
+		token = strtok(NULL, SHELL_TOKEN_DELIMITER);
+	}
+	tokens[position] = NULL;
+	return (tokens);
+}
