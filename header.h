e3074fd81c60f570f845120287abf373acefa65d
Allan70 <aabere70@gmail.com>
2022-12-26 20:17:38 -0800

    header fixed

--- header.h
+++ header.h
@@ -4,3 +4,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+//Function declarations
+int shell_cd(char **args);
+int shell_help();
+int shell_exit();
+
+char *builtin_str[] = {
+	"cd",
+	"help",
+	"exit"
+};
+
+int (*builtin_func[])(char **) = {
+	&shell_cd,
+	&shell_help,
+	&shell_exit
+}
+
+
+int shell_num_builtins();
+int shell_cd(char **args);
+int shell_execute(char **args);
+int shell_exit();
+int shell_help();
+int shell_launch(char **args);
+void shell_loop(void);
+char *shell_read_line(void);
+char **shell_split_line(char *line);
